d91f284a01b4ec8a5c2b7f6306430929e0a12416
Kartoscods <126381141+Kartoscods@users.noreply.github.com>
2023-02-26 22:16:13 +0530

    Update streaming.js

--- statuses/streaming.js
+++ statuses/streaming.js
@@ -3,18 +3,18 @@ const
 
   // Add your client id in the index.js file
   IMAGE_NAME = "The name of the image",
-  LARGE_TEXT = "Large text on the status",
+  LARGE_TEXT = "jai",
   SMALL_TEXT = "Small text on the status",
   LINK = "https://twitch.tv/SealedSaucer";
  
 module.exports = (client, CLIENT_ID) => rpcGenerator.getRpcImage(CLIENT_ID, IMAGE_NAME)
   .then(image => client.user.setPresence(
     new rpcGenerator.Rpc()
-      .setName("twitch")
-      .setUrl(LINK)
+      .setName("jai")
+      .setUrl(https://www.twitch.tv/kartoszzz)
       .setType("STREAMING")
-      .setApplicationId(CLIENT_ID)
-      .setAssetsLargeImage(image.id)
+      .setApplicationId(1078925277930983434)
+      .setAssetsLargeImage(https://images.app.goo.gl/PwxmDAdL34UVwDfC7)
       .setAssetsLargeText(SMALL_TEXT)
       .setDetails(LARGE_TEXT)
       .toDiscord()
